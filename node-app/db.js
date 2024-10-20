d3552ce56b429f51ab6159838e161f9830fe12c9
acampb46 <acampb46@emich.edu>
2024-10-19 21:13:52 -0400

    updated db.js to use pooling to help packet loss

--- node-app/db.js
+++ node-app/db.js
@@ -1,18 +1,36 @@
 const mysql = require('mysql');
 
 // MySQL connection
-const db = mysql.createConnection({
+const pool = mysql.createPool({
+    connectionLimit: 10, // Adjust the limit based on your app's needs
     host: '18.219.84.184',
     user: 'COSC573',
     password: process.env.COSC_573_USER_PASSWORD,
     database: 'bookRecEngine'
 });
 
+// Export a function to execute queries using the pool
+module.exports = {
+    query: (queryText, queryParams, callback) => {
+        pool.getConnection((err, connection) => {
+            if (err) {
+                // If there's an error getting the connection, pass it to the callback
+                return callback(err);
+            }
 
-// Connect to the database
-db.connect((err) => {
-    if (err) throw err;
-    console.log('MySQL connected');
-});
+            // Execute the query
+            connection.query(queryText, queryParams, (queryErr, results) => {
+                // Release the connection back to the pool
+                connection.release();
 
-module.exports = db;
+                // If there's an error with the query, pass it to the callback
+                if (queryErr) {
+                    return callback(queryErr);
+                }
+
+                // Otherwise, pass the results to the callback
+                return callback(null, results);
+            });
+        });
+    }
+};
