3fef3e7c8ca511a750054f671eadfbdfc1f0142c
acampb46 <acampb46@emich.edu>
2024-11-11 10:42:38 -0500

    logging for debugging in submitReview.js

--- node-app/routes/submitReview.js
+++ node-app/routes/submitReview.js
@@ -4,7 +4,12 @@ const db = require('../db');  // Import the database connection
 
 // POST route to submit a review
 router.post('/', (req, res) => {
+    if (!req.session || !req.session.user) {
+        return res.status(401).json({ error: 'User not logged in' });
+    }
     const { bookTitle, bookIsbn, rating, review } = req.body;
+
+    console.log(req.body);
 
     const query = `INSERT INTO userRatings (id, username, date, stars, review, book_isbn, book_title)
                    VALUES (?, ?, NOW(), ?, ?, ?, ?)`;
