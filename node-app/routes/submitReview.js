3e44a5138a592f97e1e4748dedd682d6a8a72a23
acampb46 <acampb46@emich.edu>
2024-11-12 12:05:22 -0500

    added handling for new userRatings table

--- node-app/routes/submitReview.js
+++ node-app/routes/submitReview.js
@@ -7,14 +7,14 @@ router.post('/', (req, res) => {
     if (!req.session.username || !req.session.userId) {
         return res.status(401).json({ error: 'User not logged in' });
     }
-    const { book_title, book_isbn, stars, review } = req.body;
+    const { book_title, book_isbn, stars } = req.body;
 
     console.log(req.body);
 
-    const query = `INSERT INTO userRatings (id, stars, review, book_isbn, book_title)
-                   VALUES (?, ?, ?, ?, ?)`;
+    const query = `INSERT INTO userRatings (id, stars, book_isbn, book_title)
+                   VALUES (?, ?, ?, ?)`;
 
-    db.query(query, [req.session.userId, stars, review, book_isbn, book_title], (err, result) => {
+    db.query(query, [req.session.userId, stars, book_isbn, book_title], (err, result) => {
         if (err) throw err;
 
         // Send a JSON response to indicate success
