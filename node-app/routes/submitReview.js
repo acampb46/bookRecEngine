7cfcee2b4f3dcb0a36b06a6930c3e4b0b991d77e
acampb46 <acampb46@emich.edu>
2024-11-11 21:47:51 -0500

    fixed variable names to get submit review query to work properly

--- node-app/routes/submitReview.js
+++ node-app/routes/submitReview.js
@@ -7,14 +7,14 @@ router.post('/', (req, res) => {
     if (!req.session.username || !req.session.userId) {
         return res.status(401).json({ error: 'User not logged in' });
     }
-    const { bookTitle, bookIsbn, rating, review } = req.body;
+    const { book_title, book_isbn, stars, review } = req.body;
 
     console.log(req.body);
 
     const query = `INSERT INTO userRatings (id, username, date, stars, review, book_isbn, book_title)
                    VALUES (?, ?, NOW(), ?, ?, ?, ?)`;
 
-    db.query(query, [req.session.userId, req.session.username, rating, review, bookIsbn, bookTitle], (err, result) => {
+    db.query(query, [req.session.userId, req.session.username, stars, review, book_isbn, book_title], (err, result) => {
         if (err) throw err;
         res.redirect('/dashboard.html');
     });
