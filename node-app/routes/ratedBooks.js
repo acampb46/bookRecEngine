00b64ab37b5ff51ce6484d095a6af6398b5a2783
acampb46 <acampb46@emich.edu>
2024-11-24 09:27:51 -0500

    changed how books were kept in list. Now easier to see which books you have rated

--- node-app/routes/ratedBooks.js
+++ node-app/routes/ratedBooks.js
@@ -6,7 +6,7 @@ const db = require('../db');
 router.get('/', async (req, res) => {
     try {
         const userId = req.session.userId;
-        const [ratedBooks] = await db.execute(`SELECT b.ISBN, b.title, r.stars
+        const [ratedBooks] = await db.query(`SELECT b.ISBN, b.title, r.stars
                                                FROM userRatings r
                                                         JOIN userBooks b ON r.book_isbn = b.ISBN
                                                WHERE r.id = 6`, [userId]);
