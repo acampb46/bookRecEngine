07895ee1dfb1bf808e7dee535ac5212808c5cd05
acampb46 <acampb46@emich.edu>
2024-11-27 11:13:07 -0500

    update join type in toReadBooks query

--- node-app/routes/toReadBooks.js
+++ node-app/routes/toReadBooks.js
@@ -6,7 +6,7 @@ const db = require('../db');
 router.get('/', async (req, res) => {
     try {
         const userId = req.session.userId;
-        const [toReadBooks] = await db.query('SELECT DISTINCT b.ISBN, b.title FROM userBooks b INNER JOIN userRatings ur ON b.userID = ur.id WHERE b.ISBN NOT IN (SELECT book_isbn from userRatings WHERE id=?)', [userId]);
+        const [toReadBooks] = await db.query('SELECT DISTINCT b.ISBN, b.title FROM userBooks b INNER JOIN userRatings ur ON b.userID = ur.id WHERE b.userID = ? AND b.ISBN NOT IN (SELECT book_isbn from userRatings WHERE id=?)', [userId, userId]);
         res.json({books: toReadBooks});
     } catch (error) {
         console.error('Error fetching books to read:', error);
